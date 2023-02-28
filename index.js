c776da8b00eecdcb8e1e442b4296adee1eface8c
kost90 <kostdonc90@gmail.com>
2023-02-28 11:50:40 +0000

    created index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const a = +prompt('What is first number?')
+const b = +prompt('What is second number?')
+
+alert(`If you plus your numbers, will be ${a + b}`);
+alert(`If you minus your numbers, will be ${a - b}`);
+alert(`If you multiply your numbers, will be ${a * b}`);
+alert(`If you divide your numbers, will be ${a / b}`);
